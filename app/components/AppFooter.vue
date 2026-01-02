4e0ed2776be9bf6fab66c0db74d3e3a8383f29e7
Omid Reza Heidari <omid.orh@gmail.com>
2026-01-02 08:55:03 -0500

    sync

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -5,9 +5,9 @@ const currentYear = format(new Date(), 'yyyy');
 </script>
 <template>
   <div class="mt-2 flex items-center justify-center gap-1 text-xs text-neutral-400 mb-4 mt-3">
-    <a class="font-semibold transition hover:text-white" href="/">AI4DMRG</a>
+    <a class="font-semibold transition hover:text-white" href="./">AI4DMRG</a>
     - by
-    <a class="font-semibold transition hover:text-white" href="/about">AI4DM Reading Group Team</a>
+    <a class="font-semibold transition hover:text-white" href="./about">AI4DM Reading Group Team</a>
     {{ currentYear }}
   </div>
 </template>
