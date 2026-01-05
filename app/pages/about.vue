4d1a50521ce71313953c92d244cc032aa91b49dd
Omid Reza Heidari <omid.orh@gmail.com>
2026-01-05 01:15:47 -0500

    refactoring

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -2,41 +2,34 @@
 const people = [
   {
     "name": "Yassine Yaakoubi",
-    "description": "Yassine Yaakoubian is an Assistant Professor at the Gina Cody School of Engineering and Computer Science at Concordia University leading a research group in AI for Decision Making (AI4DM). His research interests lie at the intersection of optimization, learning, and simulation techniques for large-scale, real-world decision-making.",
+    "description": "Yassine is an Assistant Professor at the Gina Cody School of Engineering and Computer Science at Concordia University leading a research group in AI for Decision Making (AI4DM). His research interests lie at the intersection of optimization, learning, and simulation techniques for large-scale, real-world decision-making.",
     "image": "https://yaakoubi.github.io/images/Avatar.jpg",
   },
   {
     "name": "Omid Reza Heidari",
-    "description": "Omid Reza a PhD student at Concordia University supervised by Yassine Yaakoubi and Blake Richards. His area of interest includes Multimodal Vision-Language, Large Language Models, Multimodal AI Agents, and Reinforcement Learning.",
+    "description": "Omid Reza is a PhD student at Concordia University supervised by Yassine Yaakoubi. His area of interest includes Multimodal Vision-Language, Large Language Models, Multimodal AI Agents, and Reinforcement Learning.",
     "image": "https://omid-reza.github.io/images/profile.jpg",
   },
   {
     "name": "Manar Hamed",
-    "description": "TBW",
+    "description": "Manar is a PhD student at Concordia University supervised by Yassine Yaakoubi.",
     "image": "https://mila.quebec/sites/default/themes/mila_v1/assets/images/member/no-picture-v3.jpg",
   },
   {
     "name": "Diaa Azzam",
-    "description": "TBW",
+    "description": "Diaa is a PhD student at Concordia University supervised by Yassine Yaakoubi.",
     "image": "https://mila.quebec/sites/default/themes/mila_v1/assets/images/member/no-picture-v3.jpg",
   },
   {
     "name": "Mahmoud Khaled",
-    "description": "TBW",
+    "description": "Mahmoud is a PhD student at Concordia University supervised by Yassine Yaakoubi.",
     "image": "https://mahmoud-khaled-a.github.io/images/profile.png",
   },
 ]
 </script>
 <template>
   <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 p-4">
-    <UPageCard
-        v-for="person in people"
-        :key="person.name"
-        :title="person.name"
-        :description="person.description"
-        orientation="vertical"
-        reverse
-    >
+    <UPageCard v-for="person in people" :key="person.name" :title="person.name" :description="person.description" orientation="vertical"  reverse >
       <img :src="person.image" :alt="'Image of ' + person.name" class="w-full h-100 object-cover object-top rounded-sm" />
     </UPageCard>
   </div>
